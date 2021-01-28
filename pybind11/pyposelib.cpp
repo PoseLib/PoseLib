6e37411d21d07b2450a40926a9f53b2f380afed4
Ignacio Rocco <ignacio.rocco@inria.fr>
2021-01-28 17:15:12 +0100

    added bindings for more solvers

--- pybind11/pyposelib.cpp
+++ pybind11/pyposelib.cpp
@@ -6,16 +6,44 @@
 
 
 #include <PoseLib/types.h>
+#include <PoseLib/p3p.h>
+#include <PoseLib/gp3p.h>
 #include <PoseLib/gp4ps.h>
 
 
 namespace pose_lib {
     
+
+std::vector<CameraPose> p3p_wrapper(const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X){
+    std::vector<CameraPose> output;
+    p3p(x, X, &output);
+    return output;
+}
+    
+std::vector<CameraPose> gp3p_wrapper(const std::vector<Eigen::Vector3d> &p, const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X){
+    std::vector<CameraPose> output;
+    gp3p(p, x, X, &output);
+    return output;
+}
+
 std::vector<CameraPose> gp4ps_wrapper(const std::vector<Eigen::Vector3d> &p, const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X, bool filter_solutions=true){
     std::vector<CameraPose> output;
     gp4ps(p, x, X, &output, filter_solutions);
     return output;
 }
+    
+std::vector<CameraPose> gp4ps_kukelova_wrapper(const std::vector<Eigen::Vector3d> &p, const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X, bool filter_solutions=true){
+    std::vector<CameraPose> output;
+    gp4ps_kukelova(p, x, X, &output, filter_solutions);
+    return output;
+}
+    
+std::vector<CameraPose> gp4ps_camposeco_wrapper(const std::vector<Eigen::Vector3d> &p, const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X, bool filter_solutions=true){
+    std::vector<CameraPose> output;
+    gp4ps_camposeco(p, x, X, &output);
+    return output;
+}
+
     
 }
 
@@ -30,6 +58,9 @@ PYBIND11_MODULE(poselib, m)
             .def_readwrite("alpha", &pose_lib::CameraPose::alpha);
     
   m.doc() = "pybind11 poselib";
-
+  m.def("p3p", &pose_lib::p3p_wrapper);
+  m.def("gp3p", &pose_lib::gp3p_wrapper);
   m.def("gp4ps", &pose_lib::gp4ps_wrapper);
+  m.def("gp4ps_kukelova", &pose_lib::gp4ps_kukelova_wrapper);
+  m.def("gp4ps_camposeco", &pose_lib::gp4ps_camposeco_wrapper);
 }
