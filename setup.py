50f96c3bac0a4cb66152e3df60b2309173cecada
Viktor Larsson <viktor.larsson@math.lth.se>
2023-09-22 16:23:54 +0200

    Make sure setup.py includes pybind11 in the cmake

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DPython_EXECUTABLE=' + sys.executable]
+                      '-DPython_EXECUTABLE=' + sys.executable,
+                      '-DPYTHON_PACKAGE=ON']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
