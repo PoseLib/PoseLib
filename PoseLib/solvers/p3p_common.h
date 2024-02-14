5d8c90de1f7e00695236181f7fb302dc9f4c95f4
yq <dingyaqing@163.com>
2024-02-14 22:17:25 +0100

    refine_p3p_RU3pt
    Refine the P3P solver. A new implementation for the relative pose with known rotation axis.

--- PoseLib/solvers/p3p_common.h
+++ PoseLib/solvers/p3p_common.h
@@ -5,11 +5,91 @@
 
 namespace poselib {
 
+inline double cubic_trigonometric_solution(const double alpha, const double beta, const double k2, const double alpha3) {
+    const double H = std::sqrt(-alpha3);
+    const double I = std::sqrt(-alpha / 3.0);
+    const double J = std::acos(-beta / (2.0 * H));
+    const double K = std::cos(J / 3.0);
+    return 2.0 * I * K - k2 / 3.0;  
+}
+
+ 
+inline double cubic_cardano_solution(const double beta, const double G, const double k2) {
+    const double M = std::cbrt(-0.5 * beta + sqrt(G));
+    const double N = -std::cbrt(0.5 * beta + sqrt(G));
+    return M + N - k2 / 3.0;
+}
+
+bool inline root2real(double b, double c, double &r1, double &r2) {
+    double THRESHOLD = -1.0e-12;
+    double v = b * b - 4.0 * c;
+    if (v < THRESHOLD) {
+        r1 = r2 = -0.5 * b;
+        return v >= 0;
+    }
+    if (v > THRESHOLD && v < 0.0) {
+        r1 = -0.5 * b;
+        r2 = -2;
+        return true;
+    }
+
+    double y = std::sqrt(v);
+    if (b < 0) {
+        r1 = 0.5 * (-b + y);
+        r2 = 0.5 * (-b - y);
+    } else {
+        r1 = 2.0 * c / (-b + y);
+        r2 = 2.0 * c / (-b - y);
+    }
+    return true;
+}
+
+inline std::array<Eigen::Vector3d, 2> compute_pq(Eigen::Matrix3d C) {
+    std::array<Eigen::Vector3d, 2> pq;
+    Eigen::Matrix3d C_adj;
+
+    C_adj(0, 0) = C(1, 2) * C(2, 1) - C(1, 1) * C(2, 2);
+    C_adj(1, 1) = C(0, 2) * C(2, 0) - C(0, 0) * C(2, 2);
+    C_adj(2, 2) = C(0, 1) * C(1, 0) - C(0, 0) * C(1, 1);
+    C_adj(0, 1) = C(0, 1) * C(2, 2) - C(0, 2) * C(2, 1);
+    C_adj(0, 2) = C(0, 2) * C(1, 1) - C(0, 1) * C(1, 2);
+    C_adj(1, 0) = C_adj(0, 1);
+    C_adj(1, 2) = C(0, 0) * C(1, 2) - C(0, 2) * C(1, 0);
+    C_adj(2, 0) = C_adj(0, 2);
+    C_adj(2, 1) = C_adj(1, 2);
+
+    Eigen::Vector3d v;
+    if (C_adj(0, 0) > C_adj(1, 1)) {
+        if (C_adj(0, 0) > C_adj(2, 2)) {
+            v = C_adj.col(0) / std::sqrt(C_adj(0, 0));
+        } else {
+            v = C_adj.col(2) / std::sqrt(C_adj(2, 2));
+        }
+    } else if (C_adj(1, 1) > C_adj(2, 2)) {
+        v = C_adj.col(1) / std::sqrt(C_adj(1, 1));
+    } else {
+        v = C_adj.col(2) / std::sqrt(C_adj(2, 2));
+    }
+
+    Eigen::Matrix3d D = C;
+    D(0, 1) -= v(2);
+    D(0, 2) += v(1);
+    D(1, 2) -= v(0);
+    D(1, 0) += v(2);
+    D(2, 0) -= v(1);
+    D(2, 1) += v(0);
+
+    pq[0] = D.col(0);
+    pq[1] = D.row(0);
+
+    return pq;
+}
+
 // Performs a few newton steps on the equations
 inline void refine_lambda(double &lambda1, double &lambda2, double &lambda3, const double a12, const double a13,
                           const double a23, const double b12, const double b13, const double b23) {
 
-    for (int iter = 0; iter < 5; ++iter) {
+    for (int iter = 0; iter < 0; ++iter) {
         double r1 = (lambda1 * lambda1 - 2.0 * lambda1 * lambda2 * b12 + lambda2 * lambda2 - a12);
         double r2 = (lambda1 * lambda1 - 2.0 * lambda1 * lambda3 * b13 + lambda3 * lambda3 - a13);
         double r3 = (lambda2 * lambda2 - 2.0 * lambda2 * lambda3 * b23 + lambda3 * lambda3 - a23);
--- PoseLib/solvers/p3p_ding.cc
+++ PoseLib/solvers/p3p_ding.cc
@@ -26,122 +26,17 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Author: Mark Shachkov (mark.shachkov@gmail.com)
+// Author: Yaqing Ding (yaq.ding@gmail.com). 
+// Some of the scripts are based on Mark Shachkov (mark.shachkov@gmail.com) and the Lambda-twist P3P implementation
 
 #include "p3p_ding.h"
 
 #include "p3p_common.h"
 
+
 namespace poselib {
 
-double cubic_trigonometric_solution(const double alpha, const double beta, const double k2) {
-    const double H = std::sqrt(-alpha * alpha * alpha / 27.0);
-    const double I = std::sqrt(-alpha / 3.0);
-    const double J = std::acos(-beta / (2.0 * H));
-    const double K = std::cos(J / 3.0);
-    return 2.0 * I * K - k2 / 3.0;
-}
-
-double cubic_cardano_solution(const double beta, const double G, const double k2) {
-    const double M = std::cbrt(-0.5 * beta + sqrt(G));
-    const double N = -std::cbrt(0.5 * beta + sqrt(G));
-    return M + N - k2 / 3.0;
-}
-
-std::array<Eigen::Vector3d, 2> compute_pq(const double s, const double a, const double b, const double m12,
-                                          const double m13, const double m23) {
-    std::array<Eigen::Vector3d, 2> pq;
-    Eigen::Matrix3d C, C_adj;
-
-    C(0, 0) = -a + s * (1 - b);
-    C(0, 1) = -m13 * s;
-    C(0, 2) = a * m23 + b * m23 * s;
-    C(1, 0) = -m13 * s;
-    C(1, 1) = s + 1;
-    C(1, 2) = -m12;
-    C(2, 0) = a * m23 + b * m23 * s;
-    C(2, 1) = -m12;
-    C(2, 2) = -a - b * s + 1;
-
-    C_adj(0, 0) = C(1, 2) * C(2, 1) - C(1, 1) * C(2, 2);
-    C_adj(1, 1) = C(0, 2) * C(2, 0) - C(0, 0) * C(2, 2);
-    C_adj(2, 2) = C(0, 1) * C(1, 0) - C(0, 0) * C(1, 1);
-    C_adj(0, 1) = C(0, 1) * C(2, 2) - C(0, 2) * C(2, 1);
-    C_adj(0, 2) = C(0, 2) * C(1, 1) - C(0, 1) * C(1, 2);
-    C_adj(1, 0) = C(1, 0) * C(2, 2) - C(1, 2) * C(2, 0);
-    C_adj(1, 2) = C(0, 0) * C(1, 2) - C(0, 2) * C(1, 0);
-    C_adj(2, 0) = C(1, 1) * C(2, 0) - C(1, 0) * C(2, 1);
-    C_adj(2, 1) = C(0, 0) * C(2, 1) - C(0, 1) * C(2, 0);
-
-    Eigen::Vector3d v;
-    if (C_adj(0, 0) > C_adj(1, 1)) {
-        if (C_adj(0, 0) > C_adj(2, 2)) {
-            v = C_adj.col(0) / std::sqrt(C_adj(0, 0));
-        } else {
-            v = C_adj.col(2) / std::sqrt(C_adj(2, 2));
-        }
-    } else if (C_adj(1, 1) > C_adj(2, 2)) {
-        v = C_adj.col(1) / std::sqrt(C_adj(1, 1));
-    } else {
-        v = C_adj.col(2) / std::sqrt(C_adj(2, 2));
-    }
-
-    Eigen::Matrix3d D = C;
-    D(0, 1) -= v(2);
-    D(0, 2) += v(1);
-    D(1, 2) -= v(0);
-    D(1, 0) += v(2);
-    D(2, 0) -= v(1);
-    D(2, 1) += v(0);
-
-    pq[0] = D.col(0);
-    pq[1] = D.row(0);
-
-    return pq;
-}
-
-std::pair<int, std::array<double, 2>> compute_line_conic_intersection(Eigen::Vector3d &l, const double b,
-                                                                      const double m13, const double m23) {
-    std::pair<int, std::array<double, 2>> result;
-    const double cxa = -b * l(1) * l(1) + l(2) * l(2);
-    const double cxb = -2 * b * m23 * l(1) * l(2) - 2 * b * l(0) * l(1) - 2 * m13 * l(2) * l(2);
-    const double cxc = -2 * b * m23 * l(0) * l(2) - b * l(0) * l(0) - b * l(2) * l(2) + l(2) * l(2);
-    const double d = cxb * cxb - 4 * cxa * cxc;
-
-    if (d < 0) {
-        result.first = 0;
-        return result;
-    }
-
-    result.second[0] = (-cxb + std::sqrt(d)) / (2.0 * cxa);
-    result.second[1] = (-cxb - std::sqrt(d)) / (2.0 * cxa);
-    result.first = d > 0 ? 2 : 1;
-    return result;
-}
-
-void compute_pose(const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X, const double a12,
-                  const double a13, const double a23, const double m12, const double m13, const double m23,
-                  const double x_root, const double y_root, std::vector<CameraPose> *output) {
-    double d3 = std::sqrt(a13) / std::sqrt(x_root * x_root - 2 * m13 * x_root + 1);
-    double d2 = y_root * d3;
-    double d1 = x_root * d3;
-
-    refine_lambda(d1, d2, d3, a12, a13, a23, m12, m13, m23);
-
-    Eigen::Matrix3d A, B;
-    A.col(0) = X[0] - X[1];
-    A.col(1) = X[2] - X[0];
-    A.col(2) = (X[0] - X[1]).cross(X[2] - X[0]);
-    B.col(0) = d1 * x[0] - d2 * x[1];
-    B.col(1) = d3 * x[2] - d1 * x[0];
-    B.col(2) = B.col(0).cross(B.col(1));
-
-    Eigen::Matrix3d R = B * A.inverse();
-    Eigen::Vector3d t = d1 * x[0] - R * X[0];
-    output->emplace_back(R, t);
-}
-
-int p3p_ding(const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vector3d> &X,
+int p3p_ding(const std::vector<Eigen::Vector3d> &x_copy, const std::vector<Eigen::Vector3d> &X_copy,
              std::vector<CameraPose> *output) {
     if (output == nullptr) {
         return 0;
@@ -149,55 +44,190 @@ int p3p_ding(const std::vector<Eigen::Vector3d> &x, const std::vector<Eigen::Vec
     output->clear();
     output->reserve(4);
 
-    const double a12 = (X[0] - X[1]).squaredNorm();
-    const double a13 = (X[0] - X[2]).squaredNorm();
-    const double a23 = (X[1] - X[2]).squaredNorm();
-    const double a = a12 / a23;
-    const double b = a13 / a23;
+    Eigen::Vector3d X01 = X_copy[0] - X_copy[1];
+    Eigen::Vector3d X02 = X_copy[0] - X_copy[2];
+    Eigen::Vector3d X12 = X_copy[1] - X_copy[2];
+
+    const double a12_copy = X01.squaredNorm();
+    const double a13_copy = X02.squaredNorm();
+    const double a23_copy = X12.squaredNorm();
+
+    std::vector<Eigen::Vector3d> X = X_copy;
+    std::vector<Eigen::Vector3d> x = x_copy;
+
+    double a12;
+    double a13;
+    double a23;
+
+    // Switch X,x so that BC is the largest distance among {X01, X02, X12}
+    if (a12_copy > a13_copy) {
+        if (a12_copy > a23_copy) {
+            x[0] = x_copy[2];
+            x[2] = x_copy[0];
+            X[0] = X_copy[2];
+            X[2] = X_copy[0];
+            a12 = a23_copy;
+            a13 = a13_copy;
+            a23 = a12_copy;
+            X01 = -X12;
+            X02 = -X02;
+        } else {
+            a12 = a12_copy;
+            a13 = a13_copy;
+            a23 = a23_copy;
+        }
+    } else if (a13_copy > a23_copy) {
+        x[0] = x_copy[1];
+        x[1] = x_copy[0];
+        X[0] = X_copy[1];
+        X[1] = X_copy[0];
+        a12 = a12_copy;
+        a13 = a23_copy;
+        a23 = a13_copy;
+        X01 = -X01;
+        X02 = X12;
+    } else {
+        a12 = a12_copy;
+        a13 = a13_copy;
+        a23 = a23_copy;
+    }
+
+    const double a23d = 1.0 / a23;
+    const double a = a12 * a23d;
+    const double b = a13 * a23d;
 
     const double m12 = x[0].dot(x[1]);
     const double m13 = x[0].dot(x[2]);
     const double m23 = x[1].dot(x[2]);
 
-    const double k3_inv = 1.0 / (b * (-b * m23 * m23 + b + m13 * m13 - 1));
-    const double k2 = k3_inv * (-2 * a * b * m23 * m23 + 2 * a * b + a * m13 * m13 - a - b * b * m23 * m23 + b * b +
-                                2 * b * m12 * m13 * m23 - 2 * b - m13 * m13 + 1);
-    const double k1 = k3_inv * (-a * a * m23 * m23 + a * a - 2 * a * b * m23 * m23 + 2 * a * b +
-                                2 * a * m12 * m13 * m23 - 2 * a + b * m12 * m12 - b - m12 * m12 + 1);
-    const double k0 = k3_inv * (a * (-a * m23 * m23 + a + m12 * m12 - 1));
-    const double alpha = k1 - 1.0 / 3.0 * k2 * k2;
-    const double beta = k0 - 1.0 / 3.0 * k1 * k2 + (2.0 / 27.0) * k2 * k2 * k2;
-    const double G = beta * beta / 4.0 + alpha * alpha * alpha / 27.0;
+    // Ugly parameters to simplify the calculation
+    const double m23sq = -m23 * m23 + 1.0;
+    const double m13sq = -1.0 + m13 * m13;
+    const double m12sq = -1.0 + m12 * m12;
+    const double ab = a * b;
+    const double bsq = b * b;
+    const double asq = a * a;
+    const double m123 = -2.0 + 2.0 * m12 * m13 * m23;
+    const double bsqm23sq = bsq * m23sq;
+    const double asqm23sq = asq * m23sq;
+    const double abm23sq = 2.0 * ab * m23sq;
+
+    const double k3_inv = 1.0 / (bsqm23sq + b * m13sq);
+    const double k2 = k3_inv * ((-1.0 + a) * m13sq + abm23sq + bsqm23sq + b * m123);
+    const double k1 = k3_inv * (asqm23sq + abm23sq + a * m123 + (-1.0 + b) * m12sq);
+    const double k0 = k3_inv * (asqm23sq + a * m12sq);
+    const double k22 = k2 * k2;
+    const double alpha = k1 - 1.0 / 3.0 * k22;
+    const double beta = k0 - 1.0 / 3.0 * k1 * k2 + (2.0 / 27.0) * k22 * k2;
+    const double alpha3 = alpha * alpha * alpha / 27.0;
+    const double G = beta * beta / 4.0 + alpha3;
 
     double s;
     if (G != 0) {
         if (G < 0) {
-            s = cubic_trigonometric_solution(alpha, beta, k2);
+            s = cubic_trigonometric_solution(alpha, beta, k2, alpha3);
         } else {
             s = cubic_cardano_solution(beta, G, k2);
         }
     } else {
         s = -k2 / 3.0 + (alpha != 0 ? (3.0 * beta / alpha) : 0);
     }
+    Eigen::Matrix3d C;
+    C(0, 0) = -a + s * (1 - b);
+    C(0, 1) = -m13 * s;
+    C(0, 2) = a * m23 + b * m23 * s;
+    C(1, 0) = C(0, 1);
+    C(1, 1) = s + 1;
+    C(1, 2) = -m12;
+    C(2, 0) = C(0, 2);
+    C(2, 1) = C(1, 2);
+    C(2, 2) = -a - b * s + 1;
 
-    std::array<Eigen::Vector3d, 2> pq = compute_pq(s, a, b, m12, m13, m23);
+    std::array<Eigen::Vector3d, 2> pq = compute_pq(C);
 
-    for (int i = 0; i < 2; i++) {
-        auto [n_roots, x_roots] = compute_line_conic_intersection(pq[i], b, m13, m23);
-        for (int j = 0; j < n_roots; j++) {
-            const double x_root = x_roots[j];
-            const double y_root = (-pq[i](0) - pq[i](1) * x_root) / pq[i](2);
-            if (x_root <= 0 || y_root <= 0) {
+    double d1, d2, d3;
+    CameraPose pose;
+    output->clear();
+    Eigen::Matrix3d XX;
+
+    XX << X01, X02, X01.cross(X02);
+    XX = XX.inverse().eval();
+
+    Eigen::Vector3d v1, v2;
+    Eigen::Matrix3d YY;
+
+    int n_sols = 0;
+
+    for (int i = 0; i < 2; ++i) {
+        // [p1 p2 p3] * [1; x; y] = 0, or [p1 p2 p3] * [d3; d1; d2] = 0
+        double p1 = pq[i](0);
+        double p2 = pq[i](1);
+        double p3 = pq[i](2);
+        // here we run into trouble if p1 is zero,
+        // so depending on which is larger, we solve for either d1 or d2
+        // The case p1 = p2 = 0 is degenerate and can be ignored
+        bool switch_12 = std::abs(p1) <= std::abs(p2);
+
+        if (switch_12) {
+            // eliminate d1
+            double w0 = -p1 / p2;
+            double w1 = -p3 / p2;
+            double ca = 1.0 / (w1 * w1 - b);
+            double cb = 2.0 * (b * m23 - m13 * w1 + w0 * w1) * ca;
+            double cc = (w0 * w0 - 2 * m13 * w0 - b + 1.0) * ca;
+            double taus[2];
+            if (!root2real(cb, cc, taus[0], taus[1]))
                 continue;
+            for (double tau : taus) {
+                if (tau <= 0)
+                    continue;
+                // positive only
+                d3 = std::sqrt(a23 / (tau * (tau - 2.0 * m23) + 1.0));
+                d2 = tau * d3;
+                d1 = (w0 * d3 + w1 * d2);
+                if (d1 < 0)
+                    continue;
+
+                refine_lambda(d1, d2, d3, a12, a13, a23, m12, m13, m23);
+                v1 = d1 * x[0] - d2 * x[1];
+                v2 = d1 * x[0] - d3 * x[2];
+                YY << v1, v2, v1.cross(v2);
+                Eigen::Matrix3d R = YY * XX;
+                output->emplace_back(R, d1 * x[0] - R * X[0]);
+                ++n_sols;
             }
-            compute_pose(x, X, a12, a13, a23, m12, m13, m23, x_root, y_root, output);
+        } else {
+            double w0 = -p2 / p1;
+            double w1 = -p3 / p1;
+            double ca = 1.0 / (-a * w1 * w1 + 2 * a * m23 * w1 - a + 1);
+            double cb = 2 * (a * m23 * w0 - m12 - a * w0 * w1) * ca;
+            double cc = (1 - a * w0 * w0) * ca;
+
+            double taus[2];
+            if (!root2real(cb, cc, taus[0], taus[1]))
+                continue;
+            for (double tau : taus) {
+                if (tau <= 0)
+                    continue;
+                d1 = std::sqrt(a12 / (tau * (tau - 2.0 * m12) + 1.0));
+                d2 = tau * d1;
+                d3 = w0 * d1 + w1 * d2;
+
+                if (d3 < 0)
+                    continue;
+
+                refine_lambda(d1, d2, d3, a12, a13, a23, m12, m13, m23);
+                v1 = d1 * x[0] - d2 * x[1];
+                v2 = d1 * x[0] - d3 * x[2];
+                YY << v1, v2, v1.cross(v2);
+                Eigen::Matrix3d R = YY * XX;
+                output->emplace_back(R, d1 * x[0] - R * X[0]);
+                ++n_sols;
+            }
         }
 
-        if ((G == 0) | (G < 0 ? n_roots : !n_roots)) {
-            continue;
-        }
-
-        break;
+        if ((n_sols > 0 && G > 0))
+            break;
     }
 
     return output->size();
