741fddb493aa6a557002edd210c926e0dafe107b
Viktor Larsson <viktor.larsson@math.lth.se>
2024-12-14 13:34:05 +0100

    bugfix in HybridPoseEstimator

--- PoseLib/robust/estimators/hybrid_pose.h
+++ PoseLib/robust/estimators/hybrid_pose.h
@@ -41,15 +41,12 @@ class HybridPoseEstimator {
     HybridPoseEstimator(const RansacOptions &ransac_opt, const std::vector<Point2D> &points2D,
                         const std::vector<Point3D> &points3D, const std::vector<PairwiseMatches> &pairwise_matches,
                         const std::vector<CameraPose> &map_ext)
-        : opt(ransac_opt), x(points2D), X(points3D), matches(pairwise_matches), map_poses(map_ext) {
+        : num_data(points2D.size()), opt(ransac_opt), x(points2D), X(points3D), matches(pairwise_matches),
+          map_poses(map_ext) {
         rng = opt.seed;
         xs.resize(sample_sz);
         Xs.resize(sample_sz);
         sample.resize(sample_sz);
-        num_data = points2D.size();
-        for (const PairwiseMatches &m : matches) {
-            num_data += m.x1.size();
-        }
     }
 
     void generate_models(std::vector<CameraPose> *models);
@@ -57,7 +54,7 @@ class HybridPoseEstimator {
     void refine_model(CameraPose *pose) const;
 
     const size_t sample_sz = 3;
-    size_t num_data;
+    const size_t num_data;
 
   private:
     const RansacOptions &opt;
