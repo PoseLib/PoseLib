35aca7c8916cd0bb81aa2092ac2641607e0da7b8
Johan Edstedt <johan.edstedt@liu.se>
2025-07-27 18:02:33 +0900

    can we get away with no no-aligned-allocation flag on macos?

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', '') + ' -fno-aligned-allocation' if platform.system() == "Darwin" else '',
+            env.get('CXXFLAGS', ''), # + ' -fno-aligned-allocation' if platform.system() == "Darwin" else '',
             self.distribution.get_version()
         )
         if not os.path.exists(self.build_temp):
