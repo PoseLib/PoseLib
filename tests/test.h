c596417a10a8575f4ce48b4a6018e48c00e6b149
Viktor Larsson <viktor.larsson@math.lth.se>
2026-03-09 16:06:34 +0100

    Fix macro wrapping

--- tests/test.h
+++ tests/test.h
@@ -8,36 +8,36 @@
 #include <Eigen/Dense>
 
 #define REQUIRE(COND)                                                                                                  \
-    if (!(COND)) {                                                                                                     \
+    do {if (!(COND)) {                                                                                                     \
         std::cout << "Failure: " #COND " was not satisfied.\n";                                                        \
         return false;                                                                                                  \
-    }
+    } } while (0);
 #define REQUIRE_EQ(VAL1, VAL2)                                                                                         \
-    {                                                                                                                  \
+    do {                                                                                                                  \
         auto _v1 = VAL1;                                                                                               \
         auto _v2 = VAL2;                                                                                               \
         if (_v1 != _v2) {                                                                                              \
             std::cout << "Failure: " #VAL1 " was not equal to " #VAL2 ". (" << _v1 << " vs. " << _v2 << ")\n";         \
             return false;                                                                                              \
         }                                                                                                              \
-    }
+    } while(0);
 #define REQUIRE_SMALL(VAL, THR)                                                                                        \
-    {                                                                                                                  \
+    do {                                                                                                                  \
         auto _v1 = VAL;                                                                                                \
         auto _v2 = THR;                                                                                                \
         if (std::isnan(_v1) || std::abs(_v1) > _v2) {                                                                  \
             std::cout << "Failure: " #VAL " was not small " #THR ". (" << _v1 << " vs. " << _v2 << ")\n";              \
             return false;                                                                                              \
         }                                                                                                              \
-    }
+    } while(0);
 
 #define REQUIRE_M(COND, MSG)                                                                                           \
-    if (!(COND)) {                                                                                                     \
+    do { if (!(COND)) {                                                                                                     \
         std::cout << "Failure: " #COND " was not satisfied. (" + MSG + ").\n";                                         \
         return false;                                                                                                  \
-    }
+    }} while (0);
 #define REQUIRE_EQ_M(VAL1, VAL2, MSG)                                                                                  \
-    {                                                                                                                  \
+    do {                                                                                                                  \
         auto _v1 = VAL1;                                                                                               \
         auto _v2 = VAL2;                                                                                               \
         if (_v1 != _v2) {                                                                                              \
@@ -45,9 +45,9 @@
                       << ") (" + MSG + ")\n";                                                                          \
             return false;                                                                                              \
         }                                                                                                              \
-    }
+    } while(0);
 #define REQUIRE_SMALL_M(VAL, THR, MSG)                                                                                 \
-    {                                                                                                                  \
+    do {                                                                                                                  \
         auto _v1 = VAL;                                                                                                \
         auto _v2 = THR;                                                                                                \
         if (std::isnan(_v1) || std::abs(_v1) > _v2) {                                                                  \
@@ -55,7 +55,7 @@
                       << ") (" + MSG + ")\n";                                                                          \
             return false;                                                                                              \
         }                                                                                                              \
-    }
+    } while(0);
 
 static inline std::string to_string(const Eigen::MatrixXd &mat) {
     std::stringstream ss;
